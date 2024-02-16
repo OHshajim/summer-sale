95e2e7df3639928897110d9c343771d9461ea3fe
OHshajim <ajshajimmax@gmail.com>
2024-02-16 22:02:04 +0600

    calculate money

--- index.js
+++ index.js
@@ -1,41 +1,53 @@
 const cards = document.querySelectorAll('.card');
-let titleCount = 1 ;
-let TotalPrice = 0 ;
+let titleCount = 1;
+let TotalPrice = 0;
 for (let index = 0; index < cards.length; index++) {
     const card = cards[index];
     card.addEventListener('click', function () {
         // get values 
         const title = card.querySelector('h3').innerText
-        const price =parseFloat( card.querySelector('span').innerText.split(' ')[1])
+        const price = parseFloat(card.querySelector('span').innerText.split(' ')[1])
 
         // items title set
         const container = document.getElementById('title-container');
-        const p = document.createElement('p') ;
-        p.innerText =titleCount +'. '+ title ;
+        const p = document.createElement('p');
+        p.innerText = titleCount + '. ' + title;
         container.appendChild(p);
-        titleCount++ ;
+        titleCount++;
 
         // calculate price 
-        TotalPrice +=price;
+        TotalPrice += price;
         const total = document.getElementById('totalPrice')
-        total.innerText = TotalPrice ;
+        total.innerText = TotalPrice;
+        const discount = document.getElementById('discountPrice')
+        discount.innerText = 0;
+        const totalMoney = document.getElementById('total')
+        totalMoney.innerText = TotalPrice
 
         // discount part
         const applyBtn = document.getElementById('apply-btn')
-        applyBtn.addEventListener('click',function(){
+        applyBtn.addEventListener('click', function () {
             const discountText = document.getElementById('input-field')
-            // discount apply
-            if(discountText.value === 'SELL 200'){
-                // discount calculate
-                let DiscountMoney =  TotalPrice*0.2
-                const discount = document.getElementById('discountPrice')
 
-                // TOTAL MONEY Calculate
-                const totalMoney = document.getElementById('total')
-                discount.innerText = DiscountMoney ;
-                totalMoney.innerText =  TotalPrice - DiscountMoney ;
-            }    
+            // discount apply
+            if (TotalPrice >= 200) {
+                if (discountText.value === 'SELL 200') {
+                    // discount calculate
+                    let DiscountMoney = TotalPrice * 0.2
+
+                    // TOTAL MONEY Calculate
+                    discount.innerText = DiscountMoney;
+                    totalMoney.innerText = TotalPrice - DiscountMoney;
+
+                    // discountText.value = '';
+                }
+            }
+            else {
+                alert('please purchase more than 200$')
+                discountText.value = '';
+            }
+
         })
-        
+
     })
 };
